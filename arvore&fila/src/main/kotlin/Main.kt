ba12acc31804308a1f6980a5525df8f64cb51e7e
Kahilo Massango <kahilo42@gmail.com>
2024-06-20 17:16:11 +0100

    refactorando

---
+++ arvore&fila/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.application
+import screen.App
+
+fun main() = application {
+    val windowState = WindowState(placement = WindowPlacement.Fullscreen)
+    Window(onCloseRequest = ::exitApplication, state = windowState) {
+        MaterialTheme {
+            Surface(
+                modifier = Modifier,
+                color = MyTheme.background,
+            ) {
+
+                App()
+            }
+        }
+    }
+}
---
+++ arvore&fila/src/main/kotlin/MyTheme.kt
@@ -0,0 +1,23 @@
+import androidx.compose.ui.graphics.Color
+
+object MyTheme {
+
+    val primary = Color(0xFFEF3651)
+    val onPrimary = Color(0xFFF5F5F5)
+
+    val background = Color(0xFF1E1F28)
+    val onBackgroung = Color(0xFFF6F6F6)
+
+    val secondaryContainer = Color(0xFF2A2C36)
+    val onSecondaryContainer = Color(0xFFF6F6F6)
+
+    val secondary = Color(0xFF2A2C36)
+    val onSecondary = Color(0xFFF5F5F5)
+
+    val onSurfaceVariant = Color(0xFFABB4BD)
+    val outlineVariant = Color(0xFFABB4BD)
+
+    val error = Color(0xFFFF2424)
+        val onError = Color.White
+
+}
