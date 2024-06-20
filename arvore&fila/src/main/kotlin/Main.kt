731e4e019ec7de9856172d3f901488f631a52487
Kahilo Massango <kahilo42@gmail.com>
2024-06-20 16:31:52 +0100

    refatorando

---
+++ arvore&fila/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.application
+import screen.App
+import screen.QueueScreen
+
+fun main() = application {
+    val windowState = WindowState(placement = WindowPlacement.Fullscreen)
+    Window(onCloseRequest = ::exitApplication, state = windowState) {
+        MaterialTheme {
+            Surface(
+                modifier = Modifier,//.verticalScroll(rememberScrollState()),
+                color = MyTheme.background,
+               // contentColor = MyTheme.onBackgroung
+            ) {
+               // QueueScreen()
+               // LazyVerticalGridDemo()
+                App()
+            }
+        }
+    }
+}
---
+++ arvore&fila/src/main/kotlin/resources/queue/QueueItem.kt
@@ -0,0 +1,48 @@
+package resources.queue
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun QueueItem(
+    modifier: Modifier = Modifier,
+    index: Int,
+    value: Any
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "${index + 1}°",
+            fontSize = 20.sp,
+            color = MyTheme.onBackgroung
+        )
+        Card(
+            backgroundColor = MyTheme.secondaryContainer,
+            modifier = Modifier
+                //.padding(4.dp)
+                .fillMaxWidth(),
+            elevation = 8.dp,
+        ) {
+            Text(
+                text = value.toString(),
+                fontWeight = FontWeight.Medium,
+                fontSize = 22.sp,
+                color = MyTheme.onSecondaryContainer,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+    }
+}
